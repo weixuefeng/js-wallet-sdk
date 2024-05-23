534a79f86d644c43ce99703ce50f0b1ddf528eab
Victor-S-FE <victorzhangq@gmail.com>
2024-05-23 15:28:51 +0800

    update: handled the logic of RUNES UTXO balance and transfer amount

--- packages/coin-bitcoin/src/wallet/RunesWallet.ts
+++ packages/coin-bitcoin/src/wallet/RunesWallet.ts
@@ -4,31 +4,83 @@ import {BtcXrcTypes} from "../common";
 
 export class RunesWallet extends RuneMainWallet {
 
-    buildParamsForTranferring(senderAddr: string, receiverAddr: string, runeId: string, runeBalance: string, transferAmount: string, runeUtxo: UtxoInfo, btcUtxos: UtxoInfo[], feeRate: number) : { [key: string] : any } {
-        let remaining = BigInt(runeBalance) - BigInt(transferAmount);
+    buildParamsForTranferring(senderAddr: string, receiverAddr: string, runeId: string, transferAmount: string, runeUtxos: UtxoInfo[], btcUtxos: UtxoInfo[], feeRate: number) : { [key: string] : any } {
+        let toAmount = BigInt(transferAmount);  
+        let inputRuneList = [];
+        let outputRuneList = [];
+        for (let k = 0; k < runeUtxos.length; k++) {
+            let utxo = runeUtxos[k] as any;
+            if (utxo.runes && utxo.runes.length > 0) {
+                let rune = utxo.runes[0];
+                let balance = BigInt(rune.amount);
+                if (utxo.runes.length == 1 && rune.runeid == runeId && balance >= toAmount) {  
+                    inputRuneList.push({ 
+                        txId: utxo.txId, 
+                        vOut: utxo.vOut,
+                        amount: utxo.amount,
+                        address: senderAddr,
+                        data: [{"id": runeId, "amount": balance.toString()}] 
+                    });
+                    outputRuneList.push({  
+                        amount: 546,
+                        address: receiverAddr,
+                        data: {"id": runeId, "amount": toAmount.toString()}
+                    });
+                    if (balance > toAmount) { 
+                        outputRuneList.push({  
+                            amount: 546,
+                            address: senderAddr,
+                            data: {"id": runeId, "amount": (balance - toAmount).toString()} 
+                        });
+                    } 
+                    break;
+                } else if (utxo.runes.length > 1) {
+                    throw new Error("The data runes is too long!");
+                }
+            } else {
+                throw new Error("No runes asset found!");
+            }
+        }
+        if (inputRuneList && inputRuneList.length == 0) {
+            let cumulativeAmount = BigInt(0);
+            for (let m = 0; m < runeUtxos.length; m++) {
+                let e = runeUtxos[m] as any;
+                if (e.runes && e.runes.length > 0) {
+                    let rune = e.runes[0];
+                    let balance = BigInt(rune.amount);
+                    if (rune.runeid == runeId) {
+                        if (toAmount >= cumulativeAmount) {
+                            cumulativeAmount += balance;
+                            inputRuneList.push({ 
+                                txId: e.txId, 
+                                vOut: e.vOut,
+                                amount: e.amount,
+                                address: senderAddr,
+                                data: [{"id": runeId, "amount": balance.toString()}] 
+                            });
+                        } else { 
+                            outputRuneList.push({  
+                                amount: 546,
+                                address: receiverAddr,
+                                data: {"id": runeId, "amount": toAmount.toString()} 
+                            });
+                            if (cumulativeAmount > toAmount) { 
+                                outputRuneList.push({  
+                                    amount: 546,
+                                    address: senderAddr,
+                                    data: {"id": runeId, "amount": (cumulativeAmount - toAmount).toString()}
+                                });
+                            } 
+                            break;
+                        }
+                    }
+                }
+            }
+        }
         let params: any = {
             type: BtcXrcTypes.RUNEMAIN,
-            inputs: [ 
-                { 
-                    txId: runeUtxo.txId, 
-                    vOut: runeUtxo.vOut,
-                    amount: runeUtxo.amount,
-                    address: runeUtxo.address,
-                    data: [{"id": runeId, "amount": runeBalance}] 
-                },  
-            ],
-            outputs: [
-                { 
-                    address: receiverAddr,
-                    amount: 546,
-                    data: {"id": runeId, "amount": transferAmount} 
-                },
-                { 
-                    address: senderAddr,
-                    amount: 546,
-                    data: {"id": runeId, "amount": remaining.toString()} 
-                },
-            ],
+            inputs: inputRuneList,
+            outputs: outputRuneList,
             address: senderAddr,
             feePerB: feeRate,
             runeData: {
@@ -42,32 +94,25 @@ export class RunesWallet extends RuneMainWallet {
                 txId: utxo.txId,
                 vOut: utxo.vOut,
                 amount: utxo.amount,
-                address: utxo.address,
+                address: senderAddr,
             });
         }
         return params;
     }
 
-    buildMintingParams(from: string, to: string, runeId: string, amountToMint: string, timeOfRepetition: number, gasUtxos: UtxoInfo[], feeRate: number) { 
+    // buildMintingParams(from: string, to: string, runeId: string, amountToMint: string, timeOfRepetition: number, gasUtxos: UtxoInfo[], feeRate: number) { 
 
-    } 
+    // } 
 
-    buildEtchingParams(from: string, runeInfo: EtchRuneInfo, gasUtxos: UtxoInfo[], feeRate: number) { 
+    // buildEtchingParams(from: string, runeInfo: EtchRuneInfo, gasUtxos: UtxoInfo[], feeRate: number) { 
         
-    }
+    // }
 }
 
 
 export interface UtxoInfo {
     txId: String; 
     vOut: Number;
-    amount: Number;
-    address: String; 
+    amount: Number; 
 }
 
-export interface EtchRuneInfo { 
-    spacedRune: String; 
-    symbol: String | null; 
-    supply: String;
-    amountPerMint: String;
-}
